db494d2f6eb707deccca568a3656c13825d2717d
Gülşen <gulsenk48@hotmail.com>
2024-10-20 18:55:30 +0300

    Add project files.

---
+++ Library.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    public class Library 
+    {
+        //A new class is defined with "Class". It is created to bring objects together.
+
+        public string name; 
+        public string writername;
+        public string writersurname;
+        public string pagenumber;
+        public string publishhouse;
+        public DateTime recorddate;
+
+        public string Name { get { return name; } set { name = value; }}
+        public string WriterName { get { return writername; } set { writername = value; }}
+        public string Writersurname { get { return writersurname; } set { writersurname = value; }}
+        public string Pagenumber { get { return pagenumber; } set { pagenumber = value; }}
+        public string Publishhouse { get { return publishhouse; } set { publishhouse = value; }}
+        public DateTime Recorddate { get { return recorddate; } set { recorddate = value; }}
+
+       // Properties allow us to control access to data within classes.
+
+        public Library() //Default constructor.
+        {
+            recorddate = DateTime.Now;
+        }
+
+        public Library(string name, string writername, string writersurname, string pagenumber, string publishhouse) //Alternative constructor.
+        {
+            Name = name;
+            this.writername = writername;
+            Writersurname = writersurname;
+            Pagenumber = pagenumber;
+            Publishhouse = publishhouse;
+            Recorddate = DateTime.Now;
+        }
+
+        //Constructor is used to set the initial state of the class and perform the necessary initialization operations during the instantiation process.
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using library;
+
+//For default constructor.
+Library firstBook = new Library(); //Assignments were made with "New". New is used to create a new object.
+firstBook.Name = "Adı Aylin";
+firstBook.writername = "Ayşe";
+firstBook.writersurname = "KULİN";
+firstBook.pagenumber = "398";
+firstBook.publishhouse = "Remzi Kitabevi";
+
+Console.WriteLine(firstBook.name + " book named written by " + firstBook.writername + " " + firstBook.writersurname + ", page number is: " + firstBook.pagenumber + ", publish house is: " + firstBook.publishhouse + " and publish date is: " +  DateTime.Now);
+
+Console.WriteLine("-------------------------");
+
+//For alternative constructor.
+Library secondBook = new Library("Adı Aylin","Ayşe","KULİN","398","Remzi Kitabevi");
+Console.WriteLine(secondBook.name + " book named written by " + secondBook.writername + " " + secondBook.writersurname + ", page number is: " + secondBook.pagenumber + ", publish house is: " + secondBook.publishhouse + " and publish date is: " + DateTime.Now);
